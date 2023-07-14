3e802d0288c4ff51f258568b25e5adf0debc74b6
Xelemir <gruttefien@gmail.com>
2023-07-14 13:04:48 +0200

    paypal test done

--- app/premium_routes.py
+++ app/premium_routes.py
@@ -8,7 +8,7 @@ premium_route = Blueprint("Premium", "Premium", template_folder=templates_path)
 
 # Configure PayPal SDK
 set_config({
-    'mode': 'sandbox',
+    'mode': 'live',
     'client_id': paypal_client_id,
     'client_secret': paypal_client_secret
 })
