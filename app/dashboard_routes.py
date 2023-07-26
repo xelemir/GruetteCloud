cae58e197be3f4f6d5c85fa86b105bcb32cd8f18
Xelemir <gruttefien@gmail.com>
2023-07-26 02:53:24 +0200

    improve dashboard

--- app/dashboard_routes.py
+++ app/dashboard_routes.py
@@ -4,7 +4,7 @@ import os
 
 from pythonHelper import EncryptionHelper, SQLHelper
 from pythonHelper import IconHelper
-from config import url_prefix, templates_path, admin_users, gruetteStorage_path
+from config import url_prefix, templates_path, admin_users, gruetteStorage_path, logfiles_path, local_ip
     
 dashboard_route = Blueprint("Dashboard", "Dashboard", template_folder=templates_path)
 
@@ -46,8 +46,20 @@ def dashboard():
     used_space_unformatted = sum(os.path.getsize(os.path.join(gruetteStorage_path, item)) for item in os.listdir(gruetteStorage_path))
     used_space = get_formatted_file_size(used_space_unformatted)
     used_space_percent = (used_space_unformatted / (8 * 1073741824)) * 100  # 8 GB
+
+    all_users = sql.readSQL(f"SELECT username, email, has_premium FROM gruttechat_users")
     
-    return render_template('dashboard.html', url_prefix=url_prefix, username=session['username'], used_space=used_space, used_space_percent=used_space_percent, platform_message=platform_message)
+    log_lines = []
+    filtered_log_lines = []
+    with open(f"{logfiles_path}access.log", 'r') as file:
+        log_lines = file.read().splitlines()
+    log_lines.reverse()
+
+    for entry in log_lines:
+        if str(local_ip) not in entry:
+            filtered_log_lines.append(entry)
+    
+    return render_template('dashboard.html', url_prefix=url_prefix, username=session['username'], used_space=used_space, used_space_percent=used_space_percent, platform_message=platform_message, all_users=all_users, events=filtered_log_lines)
 
 @dashboard_route.route('/dashboard/createstatusmessage', methods=['POST'])
 def create_status_message():
@@ -73,5 +85,38 @@ def delete_status_message():
     sql = SQLHelper.SQLHelper()
 
     sql.writeSQL(f"DELETE FROM gruttechat_platform_messages")
+
+    return redirect(f'{url_prefix}/dashboard')
+
+@dashboard_route.route('/dashboard/deleteuser/<username>')
+def delete_user(username):
+    if 'username' not in session or session['username'] not in admin_users:
+        return redirect(f'{url_prefix}/')
+
+    sql = SQLHelper.SQLHelper()
+
+    sql.writeSQL(f"DELETE FROM gruttechat_users WHERE username = '{username}'")
+
+    return redirect(f'{url_prefix}/dashboard')
+
+@dashboard_route.route('/dashboard/giftplus/<username>')
+def gift_plus(username):
+    if 'username' not in session or session['username'] not in admin_users:
+        return redirect(f'{url_prefix}/')
+
+    sql = SQLHelper.SQLHelper()
+
+    sql.writeSQL(f"UPDATE gruttechat_users SET has_premium = {True} WHERE username = '{username}'")
+
+    return redirect(f'{url_prefix}/dashboard')
+
+@dashboard_route.route('/dashboard/revokeplus/<username>')
+def revoke_plus(username):
+    if 'username' not in session or session['username'] not in admin_users:
+        return redirect(f'{url_prefix}/')
+
+    sql = SQLHelper.SQLHelper()
+
+    sql.writeSQL(f"UPDATE gruttechat_users SET has_premium = {False} WHERE username = '{username}'")
 
     return redirect(f'{url_prefix}/dashboard')
