c2999187c72cf38143a38892d9cdc2096e5d04a1
Xelemir <gruttefien@gmail.com>
2023-07-28 21:00:05 +0200

    GrütteChat now works with websocket

--- app/chat_routes.py
+++ app/chat_routes.py
@@ -1,9 +1,8 @@
-from flask import render_template, request, redirect, session, jsonify, Blueprint, url_for
+from flask import render_template, request, redirect, session, Blueprint
 import logging
-import hashlib
 
 from pythonHelper import EncryptionHelper, OpenAIWrapper, SQLHelper
-from config import url_prefix, templates_path, admin_users
+from config import url_prefix, templates_path
 
 
 eh = EncryptionHelper.EncryptionHelper()
@@ -12,96 +11,62 @@ eh = EncryptionHelper.EncryptionHelper()
 chat_route = Blueprint("Chat", "Chat", template_folder=templates_path)
 
 
-"""@chat_route.route("/get_messages", methods=["GET"])
-def get_messages():
+@chat_route.route("/ai/<method>", methods=["POST", "GET"])
+def send(method):
+    """ AI Chat route
 
-     
+    Args:
+        method (str): The method to be executed
+
+    Returns:
+        str: The rendered template or a redirect
+    """
+    
     if "username" not in session:
         return redirect(f"{url_prefix}/")
-    
+
+    ai = OpenAIWrapper.OpenAIWrapper()
     sql = SQLHelper.SQLHelper()
-    username = str(request.args.get("username")).lower()
-    recipient = str(request.args.get("recipient")).lower()
-    messages_list = []
     
-    # Fetch all messages from the database
-    get_messages = sql.readSQL(f"SELECT * FROM gruttechat_messages WHERE username_send = '{username}' AND username_receive = '{recipient}' OR username_send = '{recipient}' AND username_receive = '{username}' ORDER BY created_at DESC")
-    
-    for message in get_messages:
-        # Decrypt the message
-        try:
-            decrypted_message = str(eh.decrypt_message(message["message_content"]))
-        except:
-            decrypted_message = "Decryption Error!"
+    # Get chat history from session
+    chat_history = session.get("chat_history", [])
 
-        # Check if the message was sent by the user or the recipient abd add it to the list
-        if message["username_send"] == username:
-            messages_list.append(["You", decrypted_message])
-        else:
-            messages_list.append([recipient, decrypted_message])
-        
-    # Return the list as JSON
-    return jsonify(messages=messages_list)
-
-
-@chat_route.route('/chat/<recipient>', methods=['GET', 'POST'])
-def chat_with(recipient):
-    
-
-    if "username" not in session:
+    # Check if the method is back, clear chat history and redirect to home
+    if method == "back":
+        session.pop("chat_history", None)
         return redirect(f"{url_prefix}/")
     
-    recipient = str(recipient).lower()
+    # Check if the method is restart, clear chat history and redirect to AI chat
+    elif method == "restart":
+        session.pop("chat_history", None)
+        return redirect(f"{url_prefix}/ai/chat")
 
-    sql = SQLHelper.SQLHelper()
-    username = str(session['username'])
-    messages_list = []
-
-    # Check if the recipient exists
-    search_recipient = sql.readSQL(f"SELECT * FROM gruttechat_users WHERE username = '{str(recipient)}'")
-    if search_recipient == []:
-        return redirect(f'{url_prefix}/chat')
-    
-    # Get the user from the database
-    user = sql.readSQL(f"SELECT * FROM gruttechat_users WHERE username = '{username}'")
-    if user == []:
-        return redirect(f'{url_prefix}/chat')
-    
-    # Post is used to send a message
-    if request.method == 'POST':
+    # Check if new message is sent, then append it to chat history, get AI response, and refresh page
+    elif "send" in request.form and request.form["message"] != "":
+        chat_history.append({"role": "user", "content": request.form["message"]})
         
-        # Check if the message is empty or too long
-        if request.form['message'] == '' or len(request.form['message']) > 1000:
-            print(f"Invalid message: {request.form['message']}")
-            
-            return redirect(f'{url_prefix}/chat/{recipient}')
-
-        # If the message is valid, encrypt it and send it to the database 
+        # Get user selected AI personality from database
+        user = sql.readSQL(f"SELECT ai_personality, has_premium FROM gruttechat_users WHERE username = '{session['username']}'")
+        if user == []:
+            selected_ai_personality = "Default"
+            has_premium = False
         else:
-            encypted_message = str(eh.encrypt_message(request.form['message']))
-            sql.writeSQL(f"INSERT INTO gruttechat_messages (username_send, username_receive, message_content) VALUES ('{username}', '{str(recipient)}', '{encypted_message}')")
-            return redirect(f'{url_prefix}/chat/{recipient}')
+            selected_ai_personality = user[0]["ai_personality"]
+            has_premium = bool(user[0]["has_premium"])
 
-    # Get is used to load the chat
-    get_messages = sql.readSQL(f"SELECT * FROM gruttechat_messages WHERE username_send = '{username}' AND username_receive = '{recipient}' OR username_send = '{recipient}' AND username_receive = '{username}' ORDER BY created_at DESC")
-    
-    for message in get_messages:
-        # Decrypt the message
+        # Get AI response (response is appended to chat history internally)
         try:
-            decrypted_message = str(eh.decrypt_message(message["message_content"]))
-        except:
-            decrypted_message = "Decryption Error!"
+            chat_history = ai.get_openai_response(chat_history, username=session["username"], ai_personality=selected_ai_personality, has_premium=has_premium)
 
-        # Check if the message was sent by the user or the recipient abd add it to the list
-        if message["username_send"] == username:
-            messages_list.append(["You", decrypted_message])
-        else:
-            messages_list.append([recipient, decrypted_message])
+        # If there is an error, append error message to chat history
+        except Exception as e:
+            logging.error(e)
+            chat_history.append({"role": "assistant", "content": "I am having trouble connecting... Please try again later."})
             
-    if str(recipient) in admin_users:
-        verified = True
-    else:
-        verified = False
-
-    # Render the template
-    return render_template('chat.html', username=username, recipient=recipient, messages=messages_list, url_prefix = url_prefix, verified=verified)"""
+        # Save chat history to session and refresh page
+        session["chat_history"] = chat_history
+        return redirect(f"{url_prefix}/ai/chat")
+    
+    # Reverse chat history to show most recent messages first and render template
+    chat_history.reverse()
+    return render_template("aichat.html", chat_history=chat_history, url_prefix = url_prefix)
