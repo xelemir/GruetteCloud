659fd3b7da8e24c87b52863952963c23d8e0c734
Xelemir <gruttefien@gmail.com>
2023-08-27 22:56:33 +0200

    changes

--- app/tool_routes.py
+++ app/tool_routes.py
@@ -1,6 +1,6 @@
 import os
 from flask import jsonify, render_template, request, redirect, send_file, session, Blueprint, url_for
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageOps
 
 
 from pythonHelper import SQLHelper, EncryptionHelper, MailHelper, TemplateHelper
@@ -31,47 +31,66 @@ def createRender():
     else:
         device_selection = request.form['device']
         screenshot_image = request.files["file"]
+        darkMode = request.form['theme']
         
         screenshot_image.save(os.path.join(gruetteStorage_path, "GruetteCloudRenders", "screenshot.png"))
         
+        # Load the screenshot image
+        screenshot_image = Image.open(os.path.join(gruetteStorage_path, "GruetteCloudRenders", "screenshot.png"))
+
+        # Load the device frame
         device = Image.open(os.path.join(gruetteStorage_path, "GruetteCloudRenders", f"{device_selection}.png"))
-        screenshot = Image.open(os.path.join(gruetteStorage_path, "GruetteCloudRenders", "screenshot.png"))
 
-        if "Mac" not in device_selection:
-            rad = 100
-            # Add rounded corners to the smaller image
-            circle = Image.new('L', (rad * 2, rad * 2), 0)
-            draw = ImageDraw.Draw(circle)
-            draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
-            alpha = Image.new('L', screenshot.size, 255)
-            w, h = screenshot.size
-            alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
-            alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
-            alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
-            alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
-            screenshot.putalpha(alpha)
-
-        # Find the larger dimensions
+        # Load the navbar image
+        if darkMode == "true":
+            navbar_image = Image.open(os.path.join(gruetteStorage_path, "GruetteCloudRenders", "ChromeDark.png"))
+        else:
+            navbar_image = Image.open(os.path.join(gruetteStorage_path, "GruetteCloudRenders", "ChromeLight.png"))
+        
+        
+        # Check the dimensions of the images
         width1, height1 = device.size
-        width2, height2 = screenshot.size
+        width2, height2 = screenshot_image.size
 
+        # Calculate the maximum dimensions
         max_width = max(width1, width2)
         max_height = max(height1, height2)
+        
+        rad = 100
+        # Create a circle mask for rounded corners
+        circle = Image.new('L', (rad * 2, rad * 2), 0)
+        draw = ImageDraw.Draw(circle)
+        draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
+        alpha = Image.new('L', screenshot_image.size, 255)
 
-        # Create a new image with the larger dimensions and a transparent background
+        # Apply rounded corners to the screenshot image
+        alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+        alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, height2 - rad))
+        alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (width2 - rad, 0))
+        alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (width2 - rad, height2 - rad))
+        screenshot_image.putalpha(alpha)
+
+        # Create a new image with a transparent background
         result_image = Image.new('RGBA', (max_width, max_height), (0, 0, 0, 0))
 
-        # Calculate the position to center the smaller image
+        # Calculate the position to center the screenshot
         x_offset2 = (max_width - width2) // 2
         y_offset2 = (max_height - height2) // 2
 
-        # Paste the smaller image onto the new image at the centered position with rounded corners
-        result_image.paste(screenshot, (x_offset2, y_offset2))
+        # Paste the screenshot onto the new image
+        result_image.paste(screenshot_image, (x_offset2, y_offset2))
 
-        # Paste the larger image onto the new image
+        # Check if the device is an iPhone (to add rounded corners)
+        if "iPhone" in device_selection:
+            
+            # Paste the navbar image on top of the screenshot (adjust position as needed)
+            result_image.paste(navbar_image, (x_offset2, y_offset2), navbar_image)
+
+        # Paste the device frame onto the new image
         result_image.paste(device, (0, 0), device)
 
         # Save the final result
         result_image.save(os.path.join(render_path, "render.png"))
+
         
         return render_template("createRender.html", menu=th.user(session), render_created=True, renders_in_use_light=renders_in_use_light, renders_in_use_dark=renders_in_use_dark)
