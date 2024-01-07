fbea09a962e2b502dac9a4dec194565c925c8aed
Xelemir <gruttefien@gmail.com>
2024-01-07 13:06:40 +0100

    Display App Version in app

--- app/pythonHelper/TemplateHelper.py
+++ app/pythonHelper/TemplateHelper.py
@@ -1,8 +1,17 @@
 import pythonHelper.SQLHelper as SQLHelper
+import subprocess
 
 class TemplateHelper:
     def __init__(self):
         pass
+    
+    def get_last_commit_id(self):
+        try:
+            result = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+            last_commit_id = result.decode('utf-8').strip()
+            return last_commit_id
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
     
     def user(self, session):
         if "username" not in session:
@@ -12,4 +21,5 @@ class TemplateHelper:
         user = sql.readSQL(f"SELECT * FROM gruttechat_users WHERE username = '{session['username']}'")
         if user == []:
             return
-        return {"username": user[0]["username"], "pfp": user[0]["profile_picture"], "admin": bool(user[0]["is_admin"]), "premium": bool(user[0]["has_premium"])}
+        
+        return {"username": user[0]["username"], "pfp": user[0]["profile_picture"], "admin": bool(user[0]["is_admin"]), "premium": bool(user[0]["has_premium"]), "version": self.get_last_commit_id()}
