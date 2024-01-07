74437a22351ca1effcb6ae312402787dd16b5cef
Jan <68973133+xelemir@users.noreply.github.com>
2024-01-07 13:12:39 +0100

    bug fix (1/2)

--- app/pythonHelper/TemplateHelper.py
+++ app/pythonHelper/TemplateHelper.py
@@ -1,17 +1,8 @@
 import pythonHelper.SQLHelper as SQLHelper
-import subprocess
 
 class TemplateHelper:
     def __init__(self):
         pass
-    
-    def get_last_commit_id(self):
-        try:
-            result = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-            last_commit_id = result.decode('utf-8').strip()
-            return last_commit_id
-        except subprocess.CalledProcessError as e:
-            return f"Error: {e}"
     
     def user(self, session):
         if "username" not in session:
@@ -22,4 +13,4 @@ class TemplateHelper:
         if user == []:
             return
         
-        return {"username": user[0]["username"], "pfp": user[0]["profile_picture"], "admin": bool(user[0]["is_admin"]), "premium": bool(user[0]["has_premium"]), "version": self.get_last_commit_id()}
+        return {"username": user[0]["username"], "pfp": user[0]["profile_picture"], "admin": bool(user[0]["is_admin"]), "premium": bool(user[0]["has_premium"])}
