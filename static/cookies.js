5d8526b76ffeab2ce6aa9964dff0e2d55d070162
Xelemir <gruttefien@gmail.com>
2024-06-25 16:35:43 +0200

    changes

--- static/cookies.js
+++ static/cookies.js
@@ -41,9 +41,9 @@ function showCookieBanner() {
   const banner = document.getElementById('cookie-banner');
   if (isGermanUser()) {
     banner.innerHTML = `
-    <div style="position: absolute; z-index: 300; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: var(--background-tile-color); color: var(--text-color); border-radius: 30px; padding: 20px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3); flex-direction: column; justify-content: space-between; width: 600px; max-width: 80vw; max-height: 60svh; text-align: center;">
-      <h2 style="color: var(--primary-color);">Deine Daten. Deine Wahl.</h2>
-      <p style="margin-top: 20px; text-wrap: balance;">Um Dein Erlebnis auf unserer Website zu verbessern und den Traffic zu analysieren, verwenden wir funktionale und analytische Cookies. Du hast die Möglichkeit, nicht-essentielle Cookies abzulehnen. Weitere Details und die Möglichkeit, Deine Präferenzen anzupassen, findest Du in unserer <a href="privacy" style="color: var(--text-color);">Datenschutzerklärung</a>.</p>
+    <div style="position: absolute; z-index: 300; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: var(--background-tile-color); color: var(--text-color); border-radius: 30px; padding: 20px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3); flex-direction: column; justify-content: space-between; width: 600px; max-width: 80vw; max-height: 60svh; text-align: left;">
+      <h2 style="color: var(--primary-color); font-size: 24px;">Deine Daten. Deine Wahl.</h2>
+      <p style="margin-top: 20px; text-wrap: balance; font-size: 16px;">Um Deine Erfahrung auf unserer Website zu verbessern und den Datenverkehr zu analysieren, verwenden wir funktionale und analytische Cookies. Du hast die Möglichkeit, nicht wesentliche Cookies abzulehnen. Weitere Informationen und zur Anpassung Deiner Einstellungen findest Du in unserer <a href="/privacy" style="color: var(--text-color);">Datenschutzerklärung</a>.</p>
       <div style="margin-top: 20px; display: flex; justify-content: center; gap: 20px;">
         <button onclick="setCookieStatus('decline')" style="border-radius: 25px; width: 50%; padding: 10px 0; border: none; background-color: var(--secondary-color); color: var(--text-color); cursor: pointer;">Optionale Ablehnen</button>
         <button onclick="setCookieStatus('accept')" style="border-radius: 25px; width: 50%; padding: 10px 0; border: none; background-color: var(--primary-color); color: var(--white-color); cursor: pointer;">Alle Akzeptieren</button>
@@ -52,9 +52,9 @@ function showCookieBanner() {
       `;
   } else {
     banner.innerHTML = `
-    <div style="position: absolute; z-index: 300; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: var(--background-tile-color); color: var(--text-color); border-radius: 30px; padding: 20px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3); flex-direction: column; justify-content: space-between; width: 600px; max-width: 80vw; max-height: 60svh; text-align: center;">
-      <h2 style="color: var(--primary-color);">Your Data. Your Choice.</h2>
-      <p style="margin-top: 20px; text-wrap: balance;">To enhance Your experience on our website and analyze traffic, we use functional and analytical cookies. You have the option to decline non-essential cookies. For more details and to adjust your preferences, please visit our <a href="privacy" style="color: var(--text-color);">Privacy Policy</a>.</p>
+    <div style="position: absolute; z-index: 300; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: var(--background-tile-color); color: var(--text-color); border-radius: 30px; padding: 20px; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3); flex-direction: column; justify-content: space-between; width: 600px; max-width: 80vw; max-height: 60svh; text-align: left;">
+      <h2 style="color: var(--primary-color); font-size: 24px;">Your Data. Your Choice.</h2>
+      <p style="margin-top: 20px; text-wrap: balance; font-size: 16px;">To enhance your experience on our website and analyze traffic, we utilize functional and analytical cookies. You have the option to decline non-essential cookies. For more details and to adjust your preferences, please visit our <a href="/privacy" style="color: var(--text-color);">Privacy Policy</a>.</p>
       <div style="margin-top: 20px; display: flex; justify-content: center; gap: 20px;">
         <button onclick="setCookieStatus('decline')" style="border-radius: 25px; width: 50%; padding: 10px 0; border: none; background-color: var(--secondary-color); color: var(--text-color); cursor: pointer;">Decline Optional</button>
         <button onclick="setCookieStatus('accept')" style="border-radius: 25px; width: 50%; padding: 10px 0; border: none; background-color: var(--primary-color); color: var(--white-color); cursor: pointer;">Accept All</button>
