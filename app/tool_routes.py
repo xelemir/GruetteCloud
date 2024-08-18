3fc82509df6c5735f2aeada5c0c93b6454b16395
Jan <gruttefien@gmail.com>
2024-08-18 18:45:52 +0200

    changes

--- app/tool_routes.py
+++ app/tool_routes.py
@@ -380,7 +380,11 @@ def nelly_media(filename):
     elif session["username"] != "jan" and session["username"] != "nele":
         return abort(401)
     else:
-        return send_file(f"{gruettedrive_path}nelly/{filename}")
+        try:
+            return send_file(f"{gruettedrive_path}/nelly/{filename}")
+        except Exception as e:
+            print(e)
+            return abort(404)
     
 @tool_route.route("/send-date-emails", methods=["POST"])
 def send_date_emails():
