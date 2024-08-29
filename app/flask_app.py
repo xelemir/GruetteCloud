6ebbb74e1ee7c67153fc9c23a65cbba881df1841
Jan <gruttefien@gmail.com>
2024-08-29 11:54:46 +0200

    changes

--- app/flask_app.py
+++ app/flask_app.py
@@ -35,9 +35,10 @@ eh = EncryptionHelper.EncryptionHelper()
 th = TemplateHelper.TemplateHelper()
 
 
-"""@app.before_request
+@app.before_request
 def maintenanceMode():
-    if "username" not in session: return render_template('errors/maintenance.html'), 503
+    return "ok"
+    """if "username" not in session: return render_template('errors/maintenance.html'), 503
     sql = SQLHelper.SQLHelper()
     is_admin = bool(sql.readSQL(f"SELECT * FROM gruttechat_users WHERE username = '{session['username']}'")[0]["is_admin"])
     if is_admin: return
