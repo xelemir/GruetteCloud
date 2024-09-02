648c28ba855a801cba51783e196de57aee4a14ef
Jan <gruttefien@gmail.com>
2024-09-02 23:55:30 +0200

    changes

--- app/job_routes.py
+++ app/job_routes.py
@@ -14,14 +14,13 @@ def run_job():
     # Check if secret is correct
     if request.headers.get("secret") != job_secret:
         return jsonify({"status": "error", "message": "Invalid secret"}), 401
-        
-    errors = []
+       
+    # Delete old MyAI files 
+    myai_path = os.path.join("/home/jan/www/drive/", 'myai')
+    myai_files = os.listdir(myai_path)
+    myai_error = False
     
-    myai_path = os.path.join("/home/jan/wwwroot/gruettecloud/gruetteDrive/", 'myai')
-
-    files = os.listdir(myai_path)
-
-    for file in files:
+    for file in myai_files:
         # Delete files older than 14 days
         try:
             path = os.path.join(myai_path, file)
@@ -31,15 +30,30 @@ def run_job():
                 os.remove(path)
                 
         except Exception as e:
-            errors.append({"file": file, "error": str(e)})
+            myai_error = e
             print(e)
+            
+    # Clear log files daily
+    log_error = False
+
+    try:
+        with open(os.path.join("/var/log/apache2", "error.log"), "w") as file:
+            file.write("")
+        with open(os.path.join("/var/log/apache2", "access.log"), "w") as file:
+            file.write("")
+            
+    except Exception as e:
+        log_error = e
+        print(e)
     
+    # Send email to admin
+        
     mail = MailHelper.MailHelper()
     sql = SQLHelper.SQLHelper()
     
     admin = sql.readSQL(f"SELECT first_name, email FROM users WHERE username = 'jan'")[0]
     
-    html = render_template("emails/report.html", errors=errors, username=admin["first_name"], job_name="Deletion of old MyAI files", date=datetime.now().strftime("%d.%m.%Y"), time=datetime.now().strftime("%H:%M:%S"))
+    html = render_template("emails/report.html", myai_error=myai_error, log_error=log_error, username=admin["first_name"], job_name="Daily Data Purge and Optimization Task", date=datetime.now().strftime("%d.%m.%Y"), time=datetime.now().strftime("%H:%M:%S"))
     
     mail.send_email_no_template(admin["email"], "GrütteCloud Daily Report", html)
     
