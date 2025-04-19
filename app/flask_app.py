5b8ab8ee1ca3cd8ec3bca70d5d3ab2c7ad8cd568
Jan <gruttefien@gmail.com>
2025-04-19 18:15:21 +0200

    changes

--- app/flask_app.py
+++ app/flask_app.py
@@ -241,7 +241,7 @@ def chat(error=None):
 
 
 
-@app.route("/static-gruette-styles.css", methods=["GET"])
+@app.route("/new-styles", methods=["GET"])
 def static_css():
     return send_from_directory("static", "gruette-styles.css")
 
